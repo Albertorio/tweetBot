bc95fe5aa7da228678bb96fda157776b8723caaa
Luis Albertorio <albertorio.10@hotmail.com>
2014-03-19 14:25:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify ,request, render_template
 from random import randint
+from sendgrid import Mail, SendGridClient
 app = Flask(__name__)
 app.debug = True
  
 tweets = [{"text": "jinja2", "id": 1}]
  
 @app.route('/', methods=['GET'])
-def hello_world():
+def render_page():
 	return render_template('index.html', tweets=tweets)
  
 @app.route('/tweets', methods=['POST','GET'])
 def create_tweet():
 	if request.method == 'POST':	
-		return jsonify(tweets.append({"text":request.form['tweet'],"id":randint(0,256)}))
+		newtweet = {'id': randint(0, 256), 'text': request_form['tweet']}
+		tweets.append(newtweet)
+		return jsonify(newtweet)#or return tweets
+		#return jsonify(tweets.append({"text":request.form['tweet'],"id":randint(0,256)}))
 	else:
 		#obtener todos los tweets
 		return jsonify({"tweets":tweets})
@@ -32,11 +36,16 @@ def single_tweet(id):
 	else:
 		#delete single tweet
 		for tweet in tweets:
-			if tweet.id == id:
+			removed = tweet
+			tweets.remove(tweet)
+			return jsonify(removed)
+			#if tweet.id == id:
 				#tweets.remove
-				return jsonify(tweet)
+			#	return jsonify(tweet)
  
-		return jsonify({})
+		return jsonify({'error': 'not found'})
+
+#here we add the email functionality, for later
  
 if __name__ == "__main__": 
 	app.run()
